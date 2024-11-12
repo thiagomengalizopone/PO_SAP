004b44208a7fa5f7aa0533add1b3ff9c18a4f4bd
TI Zopone <ti@zopone.com.br>
2024-11-12 09:29:32 -0300

    Atualização de versão

--- POSapB1/Zopone.AddOn.PO/Model/SqlViews.cs
+++ POSapB1/Zopone.AddOn.PO/Model/SqlViews.cs
@@ -10,35 +10,35 @@ namespace Zopone.AddOn.PO.Model
             public const string Nome = "Zopone.AddOn.PO.Model.SQL.Views.ZPN_VW_DOCUMENTOSIMPOSTO.sql";
             public const string Descricao = "";
             public const TipoScript Tipo = TipoScript.View;
-            public const Int32 Versao = 200241112;
+            public const Int32 Versao = 20241112;
         }
         public static class ZPN_VW_DOCUMENTOSSEMOBRA
         {
             public const string Nome = "Zopone.AddOn.PO.Model.SQL.Views.ZPN_VW_DOCUMENTOSSEMOBRA.sql";
             public const string Descricao = "";
             public const TipoScript Tipo = TipoScript.View;
-            public const Int32 Versao = 200241112;
+            public const Int32 Versao = 20241112;
         }
         public static class ZPN_VW_LISTALOGIMPORTACAOPO
         {
             public const string Nome = "Zopone.AddOn.PO.Model.SQL.Views.ZPN_VW_LISTALOGIMPORTACAOPO.sql";
             public const string Descricao = "";
             public const TipoScript Tipo = TipoScript.View;
-            public const Int32 Versao = 200241112;
+            public const Int32 Versao = 20241112;
         }
         public static class ZPN_VW_PERCENTUALFATURAMENTO
         {
             public const string Nome = "Zopone.AddOn.PO.Model.SQL.Views.ZPN_VW_PERCENTUALFATURAMENTO.sql";
             public const string Descricao = "";
             public const TipoScript Tipo = TipoScript.View;
-            public const Int32 Versao = 200241112;
+            public const Int32 Versao = 20241112;
         }
         public static class ZPN_VW_TotalFaturadoPedido
         {
             public const string Nome = "Zopone.AddOn.PO.Model.SQL.Views.ZPN_VW_TotalFaturadoPedido.sql";
             public const string Descricao = "";
             public const TipoScript Tipo = TipoScript.View;
-            public const Int32 Versao = 200241112;
+            public const Int32 Versao = 20241112;
         }
     }
 }
