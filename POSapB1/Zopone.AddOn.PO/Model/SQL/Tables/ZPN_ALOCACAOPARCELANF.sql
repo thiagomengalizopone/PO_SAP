c420f8be8b15ec42c0efd057843c558bd21c0282
TI Zopone <ti@zopone.com.br>
2025-01-29 17:23:53 -0300

    Tabela de Alocação de Parcelas

---
+++ POSapB1/Zopone.AddOn.PO/Model/SQL/Tables/ZPN_ALOCACAOPARCELANF.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE ZPN_ALOCACAOPARCELANF
+(
+	DocEntry int,
+	ObjType int,
+	DraftKey int,
+	Parcela int,
+	Percentural decimal(16,4),
+	ValorParcela decimal(16,4),
+	CodigoAlocacao varchar(20),
+	DescricaoAlocacao varchar(250)
+)
