258e88e44d07cc8f8c81220c2e21a3e00675383e
TI Zopone <ti@zopone.com.br>
2025-02-12 10:17:06 -0300

    cração do método para exibir as alocações cadastradas

---
+++ POSapB1/Zopone.AddOn.PO/Model/SQL/Procedures/SP_ZPN_ExibeAlocacaoDocumento.sql
@@ -0,0 +1,19 @@
+﻿
+CREATE PROCEDURE SP_ZPN_ExibeAlocacaoDocumento
+    @IdPCIDocumento varchar(250)
+AS
+BEGIN
+    SELECT 
+        [Parcela],
+        [Percentual],
+        [ValorParcela],
+        [CodigoAlocacao],
+        [DescricaoAlocacao],
+        [IdPCI]
+    FROM 
+        ZPN_ALOCACAOPARCELANF
+    WHERE 
+        IdPCIDocumento = @IdPCIDocumento
+	ORDER BY
+		Parcela;
+END
--- POSapB1/Zopone.AddOn.PO/View/NotaFiscal/FrmNotaFiscal.cs
+++ POSapB1/Zopone.AddOn.PO/View/NotaFiscal/FrmNotaFiscal.cs
@@ -19,12 +19,14 @@ namespace Zopone.AddOn.PO.View.NotaFiscal
     {
         public Matrix oMtItens { get; set; }
         public Matrix oMtAlocacao { get; set; }
-
+        public DataTable DtAlocacao { get; set; }
 
         public FrmNotaFiscal() : base()
         {
             if (oForm == null)
                 return;
+
+            DtAlocacao = oForm.DataSources.DataTables.Item("DtAloc");
 
             oMtItens = (Matrix)oForm.Items.Item("MtItem").Specific;
             oMtAlocacao = (Matrix)oForm.Items.Item("MtAloca").Specific;
@@ -32,20 +34,68 @@ namespace Zopone.AddOn.PO.View.NotaFiscal
             oMtItens.AutoResizeColumns();
             oMtAlocacao.AutoResizeColumns();
 
-            CarregaDadosAlocacao();
+            CarregaDadosAlocacao("-9999");
         }
 
-        private void CarregaDadosAlocacao()
+        private void CarregaDadosAlocacao(string IdPciDocumento = "")
         {
             try
             {
-                
+                string SQL_Alocacao = $"SP_ZPN_ExibeAlocacaoDocumento '{IdPciDocumento}'";
+
+                DtAlocacao.ExecuteQuery(SQL_Alocacao);
+
+                oMtAlocacao.Columns.Item("Parcela").DataBind.Bind("DtAloc", "Parcela");
+                oMtAlocacao.Columns.Item("Percent").DataBind.Bind("DtAloc", "Percentual");
+                oMtAlocacao.Columns.Item("ValParc").DataBind.Bind("DtAloc", "ValorParcela");
+                oMtAlocacao.Columns.Item("CodAloc").DataBind.Bind("DtAloc", "CodigoAlocacao");
+                oMtAlocacao.Columns.Item("DescAloc").DataBind.Bind("DtAloc", "DescricaoAlocacao");
+                oMtAlocacao.Columns.Item("IdPCI").DataBind.Bind("DtAloc", "IdPCI");
+
+                oMtAlocacao.AutoResizeColumns();
             }
             catch (Exception ex) 
             {
                 Util.ExibeMensagensDialogoStatusBar($"Erro ao carregar dados de alocação: {ex.Message}", BoMessageTime.bmt_Medium, true, ex);
             }
         }
+        private static void CarregarDadosTela(string formUID)
+        {
+            try
+            {
+                Form oForm = Globals.Master.Connection.Interface.Forms.Item(formUID);
+
+                DBDataSource DbOINV = oForm.DataSources.DBDataSources.Item("OINV");
+
+                CarregaDadosAlocacao(DbOINV.GetValue("U_IdPCI", 0));
+
+            }
+            catch (Exception Ex)
+            {
+                Util.ExibeMensagensDialogoStatusBar($"Erro ao carregar dados na tela de Edição de NF: {Ex.Message}", BoMessageTime.bmt_Medium, true, Ex);
+            }
+        }
+
+        internal static bool Interface_FormDataEvent(BusinessObjectInfo businessObjectInfo)
+        {
+            try
+            {
+                if (businessObjectInfo.EventType == BoEventTypes.et_DATASOURCE_LOAD && !businessObjectInfo.BeforeAction)
+                {
+                    CarregarDadosTela(businessObjectInfo.FormUID);
+                }
+
+
+                return true;
+            }
+            catch (Exception Ex)
+            {
+                Util.ExibeMensagensDialogoStatusBar($"Erro ao executar métodos de dados no documento: {Ex.Message}", BoMessageTime.bmt_Medium, true, Ex);
+                return false;
+            }
+        }
+
+        
     }
 
 }
