0ba2af58d758f2267841619d85f179d27ab52e8a
TI Zopone <ti@zopone.com.br>
2025-03-11 10:27:11 -0300

    Melhoria na listagem de NF PCI

---
+++ POSapB1/Zopone.AddOn.PO/Model/SQL/Procedures/SP_ZPN_PCI_ListaNFsPCI.sql
@@ -0,0 +1,50 @@
+﻿create PROCEDURE SP_ZPN_PCI_ListaNFsPCI
+as 
+BEGIN
+
+	declare @QtdeMinutos int = -10;
+
+
+	SELECT DISTINCT 
+		OINV.DocEntry
+	FROM 
+		OINV
+	INNER JOIN OBPL ON OBPL.BPLId = OINV.BPLId
+	INNER JOIN INV1 ON INV1.DocEntry = OINV.DocEntry
+	LEFT  JOIN "@ZPN_OPRJ" ZPN_OPRJ ON ZPN_OPRJ.Code = INV1.Project
+	LEFT  JOIN sbo_taxOne.[dbo].doc ON DOC.DocType = oinv.ObjType AND DOC.DocEntry = OINV.DocEntry
+	LEFT  JOIN sbo_taxOne.[dbo].Entidade ET ON ET.id = doc.EntityId
+	LEFT JOIN sbo_taxOne.[dbo].[DocHist] HIST ON HIST.BatchId = DOC.BatchId
+	WHERE 
+		(
+			(
+				oinv.CreateDAte = cast(getdate() AS date)
+				OR oinv.UpdateDate = cast(getdate() AS date)
+			)
+		)
+	  AND 
+		(
+			(
+				hist.StatusId = 4
+				AND ISNULL(DOC.SerialNFSe, 0) <> 0
+				AND DOC.StatusId = 4
+				AND ET.CompanyDb= 'SBO_ZOPONE_ENGENHARIA'
+				AND hist.DateAdd >= DATEADD(MINUTE, @QtdeMinutos, GETDATE())
+			)
+		OR 
+		(
+			(
+				oinv.Bplid = 12
+				OR INV1.Usage = 25
+			)
+			AND 
+			(
+				oinv.CreateTS >= FORMAT(DATEADD(MINUTE, @QtdeMinutos, GETDATE()), 'HHmm')
+				OR oinv.UpdateTs >= FORMAT(DATEADD(MINUTE, @QtdeMinutos, GETDATE()), 'HHmm')
+			)
+		)
+		)
+	  AND OINV.CANCELED <> 'Y'
+	ORDER BY 
+		DocEntry ;
+end;
---
+++ POSapB1/Zopone.AddOn.PO/Model/SQL/Procedures/SP_ZPN_RetornaDadosLinhaPO.sql
@@ -0,0 +1,47 @@
+﻿Create PROCEDURE [dbo].[SP_ZPN_RetornaDadosLinhaPO]
+(
+	@DocEntry int 
+)
+AS 
+BEGIN
+	
+	SELECT 
+		VisOrder,
+		LineNum,
+		RDR1.Project,
+		U_Candidato,
+		ORDR.CardCode,
+		ORDR.CardName,
+		U_Item,
+		U_ItemFat,
+		U_DescItemFat,
+		U_Parcela,
+		LineTotal "U_Valor",
+		RDR1.U_Tipo,
+		U_DataFat,
+		U_NroNF,
+		U_DataSol,
+		RDR1.Text "U_Obs",
+		U_Bloqueado,
+		U_itemDescription,
+		U_manSiteInfo,
+		OOAT.AbsId  "AgrNo" ,
+		ooat.Descript U_DescCont,
+		RDR1.OcrCode "PCG",
+		RDR1.OcrCode2 "Obra",
+		RDR1.OcrCode3 Regional
+
+
+
+
+	FROM 
+		RDR1 
+		INNER JOIN ORDR ON ORDR.DocEntry = RDR1.DocEntry
+		LEFT JOIN "@ZPN_OPRJ" OBRA ON OBRA.Code = RDR1.Project 
+		LEFT JOIN OOAT on OOAT.AbsId = OBRA.U_CodContrato
+	WHERE 
+		RDR1.DocEntry = @DocEntry
+	order by
+		LineNum;
+	
+END;
